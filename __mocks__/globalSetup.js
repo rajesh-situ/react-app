506ff17198a5d2343e947df1ca41e08f4e7ee035
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-01-07 20:40:17 +0530

    Testing: Adding setup file to avoid repetition

--- __mocks__/globalSetup.js
+++ __mocks__/globalSetup.js
@@ -1,4 +1,4 @@
 module.exports = () => {
-    global.window = global.window || global || {};
-    window.fetch = (...args) => Promise.resolve(args);
-  };  
+  global.window = global.window || global || {};
+  window.fetch = (...args) => Promise.resolve(args);
+};
