a4cc4489ca5b0c560e12698e9e0e718c1e97bae2
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-01-09 21:14:35 +0530

    Saga: Fixing a lint error

--- src/redux/sagas/__tests__/index.saga.test.js
+++ src/redux/sagas/__tests__/index.saga.test.js
@@ -1,5 +1,5 @@
-import combinedSagas from '../index.saga';
 import sagaHelper from 'redux-saga-testing';
+import combinedSagas from '../index.saga';
 
 describe('combined saga should combine all sagas', () => {
   const it = sagaHelper(combinedSagas());
