4e0f03c7ed1cd13ad76f6456061e8a6421e2d2da
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-01-10 17:06:35 +0530

    Middleware: Adding the router middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/index.reducer';
 import rootSaga from './sagas/index.saga';
 
 const sagaMiddleware = createSagaMiddleware();
+export const history = createBrowserHistory();
 
 // setting up redux DevTools extension when creating the store
-const enhancer = composeWithDevTools(applyMiddleware(sagaMiddleware));
+const enhancer = composeWithDevTools(applyMiddleware(
+  routerMiddleware(history),
+  sagaMiddleware
+));
 
 export default function getStore(initialState = {}) {
-  const store = createStore(rootReducer, initialState, enhancer);
+  const store = createStore(rootReducer(history), initialState, enhancer);
   sagaMiddleware.run(rootSaga);
   return store;
 }
