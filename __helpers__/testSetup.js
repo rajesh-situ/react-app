93582b35a83b9a70aa9c95188503a3b37d091ace
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-01-10 17:05:12 +0530

    TestSetup: Mocking history object

--- __helpers__/testSetup.js
+++ __helpers__/testSetup.js
@@ -1,4 +1,6 @@
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
 
+jest.mock('history', () => ({ createBrowserHistory: () => ({}) }));
+
 Enzyme.configure({ adapter: new Adapter() });
