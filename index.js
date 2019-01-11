bc0504c3a51784d3ab639a001534c62a5b3409e7
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-01-11 11:17:39 +0530

    Index: Changing the store filename

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import getStore from './src/redux/Store';
-import App from './src/App';
+import getStore from './src/redux/store';
+import App from './src/App'; //eslint-disable-line
 
 const store = getStore();
 
